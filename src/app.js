9806ebb64929022099d73f19d737ef3889cd53e4
Beethove1 <96740916+Beethove1@users.noreply.github.com>
2024-03-28 01:08:46 +0000

    Commmit_1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["My friend", "My dog", "My uncle", "The driver"];
+  let action = ["ate", "drunk", "fly"];
+  let what = ["my food", "Juice", "plane"];
+  let when = ["Today", "last month", "Yesterday"];
+  let image;
+  function ranNum(num) {
+    return Math.floor(Math.random() * num);
+  }
+  let excuseGEn =
+    who[ranNum(who.length)] +
+    " " +
+    action[ranNum(action.length)] +
+    " " +
+    what[ranNum(what.length)] +
+    " " +
+    when[ranNum(when.length)];
+  console.log(excuseGEn);
+  const text = document.querySelector(".text");
+  text.innerHTML = excuseGEn;
 };
