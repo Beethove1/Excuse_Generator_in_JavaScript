5c9d79f9f387d499c326e79389326a43ee5f2fdf
Beethove1 <96740916+Beethove1@users.noreply.github.com>
2024-03-29 03:06:25 +0000

    Commit_3

--- src/app.js
+++ src/app.js
@@ -5,25 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  let who = ["My friend", "My dog", "My uncle", "The driver"];
-  let action = ["ate", "drunk", "fly"];
-  let what = ["my food", "Juice", "plane"];
-  let when = ["Today", "last month", "Yesterday"];
-  let image = [];
-  function ranNum(num) {
-    return Math.floor(Math.random() * num);
-  }
-  let excuseGEn =
-    who[ranNum(who.length)] +
-    " " +
-    action[ranNum(action.length)] +
-    " " +
-    what[ranNum(what.length)] +
-    " " +
-    when[ranNum(when.length)];
-  console.log(excuseGEn);
-  const text = document.querySelector(".text");
-  text.innerHTML = excuseGEn;
-};
+document.getElementById("executeb").addEventListener(
+  "click",
+
+  (window.onload = function() {
+    //write your code here
+    let who = ["My friend", "My dog", "My uncle", "The driver"];
+    let action = ["ate", "drunk", "fly"];
+    let what = ["my food", "Juice", "plane"];
+    let when = ["Today", "last month", "Yesterday"];
+    let tColor = ["green", "blue", "red", "rgb(0, 149, 255)"];
+    let dColor = ["green", "blue", "red", "rgb(0, 149, 255)"];
+    let image = [
+      "https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg",
+      "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg",
+      "https://www.everland-petfood.com/wp-content/uploads/AdobeStock_203576107.jpeg"
+    ];
+    function ranNum(num) {
+      return Math.floor(Math.random() * num);
+    }
+
+    let excuseGEn =
+      who[ranNum(who.length)] +
+      " " +
+      action[ranNum(action.length)] +
+      " " +
+      what[ranNum(what.length)] +
+      " " +
+      when[ranNum(when.length)] +
+      ".";
+
+    const text = document.querySelector(".text");
+    text.innerHTML = excuseGEn;
+
+    //change background color
+    let divColor = dColor[ranNum(dColor.length)];
+    document.getElementById("body").style.backgroundColor = divColor;
+    //change border color
+
+    //change text color
+    let textColor = tColor[ranNum(tColor.length)];
+    document.getElementById("body").style.color = textColor;
+  })
+);
