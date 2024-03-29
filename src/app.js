bea195cf2eaa8745accdce6f3ca3b9396b26fe93
Beethove1 <96740916+Beethove1@users.noreply.github.com>
2024-03-29 22:27:18 +0000

    End

--- src/app.js
+++ src/app.js
@@ -10,12 +10,54 @@ document.getElementById("executeb").addEventListener(
 
   (window.onload = function() {
     //write your code here
-    let who = ["My friend", "My dog", "My uncle", "The driver"];
-    let action = ["ate", "drunk", "fly"];
-    let what = ["my food", "Juice", "plane"];
-    let when = ["Today", "last month", "Yesterday"];
-    let tColor = ["green", "blue", "red", "rgb(0, 149, 255)"];
-    let dColor = ["green", "blue", "red", "rgb(0, 149, 255)"];
+    let who = ["My friend", "My dog", "My uncle", "My friend", " I am"];
+    let action = [
+      "is listening to",
+      "drank",
+      "is traveling to",
+      "reads",
+      "working on"
+    ];
+    let what = [
+      "my juice",
+      "all the food",
+      "music",
+      "Canada",
+      "a book",
+      "my project"
+    ];
+    let when = [
+      "Today",
+      "Tomorrow",
+      "Yesterday",
+      "last month",
+      "Now",
+      "Next year",
+      "This morning",
+      "Next week",
+      "This weekend",
+      "To night"
+    ];
+    let tColor = [
+      "black",
+      "blue",
+      "red",
+      "brown",
+      "firebrick3",
+      "rgb(0, 149, 255)"
+    ];
+    let dColor = [
+      "honeydew",
+      "dark slate grey",
+      "lavender",
+      "BlanchedAlmond",
+      "gainsboro",
+      "LightSlateBlue",
+      "wheat3",
+      "honeydew1",
+      "thistle3",
+      "white"
+    ];
     let image = [
       "https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg",
       "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg",
